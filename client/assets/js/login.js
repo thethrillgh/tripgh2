7904bc803aba9f9e3954ecb5795cb38ac7c547b1
Issifu Suhununu <johnjohnson0095@gmail.com>
2017-01-31 20:46:22 -0500

    added login form view

---
+++ client/assets/js/login.js
@@ -0,0 +1,15 @@
+/*------------------------------------------
+ Buying Ticket
+ ------------------------------------------*/
+$(document).ready(function () {
+    
+    if($('body').is('.login')){
+        //capitalize first letter
+        function cap(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+        var user = JSON.parse(sessionStorage.getItem("user"));
+        console.log(user)
+    }   
+});
+
